509d035c9070f2b7fafb6a581113f76be59299db
PhuongHuu <huu.phuong195@gmail.com>
2017-08-08 22:14:02 +0700

    View using blade Extends from layout/master

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,35 +1,96 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-?><!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="utf-8">
-	<title>@yield('title', 'VtigerCRM - Hệ thống quan trị quan hệ khách hàng')</title>
-</head>
-<body>
+@extends('layouts.master')
+@section('home', 'active')
+@section('content')
+<div class="container content-container">
+	<div class="row">
+		<div class="col-sm-12">
+			<ol class="breadcrumb">
+				<li>
+					<a href="#"><span class="glyphicon glyphicon-home"></span> Home</a>
+				</li>
+				<li>
+					<a href="#">Student</a>
+				</li>
+				<li class="active">Stuent Information</li>
+			</ol>
+		</div><!-- /.col-sm-12 -->
+	</div><!-- /.row -->
 
-<div id="container">
-	<h1>Welcome to CodeIgniter!</h1>
 
-	<div id="body">
-	
-		<?php $data = Request::getName(); ?>
-		{!! print_r($data) !!}
-		<form action="store" method="POST" role="form">
-			<legend>Form title</legend>
-		
-			<div class="form-group">
-				<label for="">label</label>
-				<input type="text" class="form-control" name="username" placeholder="Input field">
+	<div class="row">
+		<div class="col-sm-4">
+			<div class="panel panel-default">
+				<div class="panel-heading">
+					<h3 class="panel-title">Student Infomation</h3>
+				</div>
+				<div class="panel-body">
+					<form action="" method="POST" role="form">
+						<div class="form-group">
+							<label for="">Full name*:</label>
+							<input type="text" class="form-control" id="" placeholder="Input field">
+						</div>
+
+						<div class="form-group">
+							<label for="">Email*:</label>
+							<input type="text" class="form-control" id="" placeholder="Input field">
+						</div>
+
+
+						<div class="form-group">
+							<label for="">Gender:</label>
+							<div class="radio">
+								<label class="radio-inline">
+									<input type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1"> 1
+								</label>
+								<label class="radio-inline">
+									<input type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2"> 2
+								</label>
+							</div>
+						</div>
+
+						<div class="form-group">
+							<label for="">Hobbies:</label>
+							<div class="radio">
+								<label class="checkbox-inline">
+								<input type="checkbox" id="inlineCheckbox1" value="option1"> Game
+								</label>
+								<label class="checkbox-inline">
+									<input type="checkbox" id="inlineCheckbox2" value="option2"> Book
+								</label>
+								<label class="checkbox-inline">
+									<input type="checkbox" id="inlineCheckbox3" value="option3"> Music
+								</label>
+
+								<label class="checkbox-inline">
+									<input type="checkbox" id="inlineCheckbox3" value="option3"> Other
+								</label>
+							</div>
+						</div>
+					</form>
+				</div>
+
+				<div class="panel-footer">
+					<button type="reset" class="btn btn-default">Cancel</button>
+					<button type="submit" class="btn btn-primary">Save</button>
+				</div><!-- /.panel-footer -->
 			</div>
-		
-			
-		
-			<button type="submit" class="btn btn-primary">Submit</button>
-		</form>
-    </div>
+		</div><!-- /.col-sm-4 -->
 
-</div>
 
-</body>
-</html>
+		<div class="col-sm-8">
+			<div class="panel panel-default">
+				<div class="panel-heading">
+					<h3 class="panel-title">List student</h3>
+				</div>
+				<div class="panel-body">
+					Panel content
+				</div>
+			</div>
+		</div>
+	</div><!-- /.row -->
+</div><!-- /.container -->
+@endsection
+
+
+
+
